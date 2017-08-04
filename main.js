f70deda94953b1f2ca8acba91b9c3a584e042c01
Bethany Priode <bpriode@gmail.com>
2017-08-03 22:18:54 -0400

    Thursday

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("hello");
+
+Memory  = "0";      // initialise memory variable
+Current = "0";      //   and value of Display ("current" value)
+Operation = 0;      // Records code for eg * / etc.
+MAXLENGTH = 30;
